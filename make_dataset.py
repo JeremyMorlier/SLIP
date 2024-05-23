73aea617788de5607b5c55e585f70a29c5c391aa
[jérémy morlier] <[jeremy.morlier@imt-atlantique.fr]>
2024-05-23 17:18:43 +0200

    custom modifications to make_dataset.py (argsparse)

--- make_dataset.py
+++ make_dataset.py
@@ -3,53 +3,68 @@
 
 # This source code is licensed under the license found in the
 # LICENSE file in the root directory of this source tree.
+import os
 import numpy as np
 import pickle
 import re
 from urllib.parse import unquote
 from tqdm import tqdm
 
+def get_args_parser(add_help=True):
+    import argparse
 
-DATASET = 'yfcc100m_dataset.txt'
+    parser = argparse.ArgumentParser(description="PyTorch Classification Training", add_help=add_help)
 
-cleanhtml = re.compile('<a.*?>|</a>|<b>|</b>|<i>|</i>')
-cleanurl = re.compile('http\S+|www\S+')
+    parser.add_argument("--metadata", default="yfcc100m_dataset", type=str, help="YFCC100M metadata file path")
+    parser.add_argument("--images_ids", type=str, default="flickr_unique_ids", help="SLIP flickr images ids")
+    parser.add_argument("--yfcc15m_tsv", type=str, default="yfcc100m_subset_data.tsv", help="OpenAI YFCC100M subset: YFCC15M")
+    parser.add_argument("--output", type=str, default="temp/", help="output pickle directory")
 
-print('=> loading YFCC image ids')
-image_ids = np.load('flickr_unique_ids.npy')
-image_ids = set(image_ids)
+    return parser
 
-print('=> loading CLIP image ids')
-clip_ids = set()
-with open('yfcc100m_subset_data.tsv') as f:
-    for l in tqdm(f.readlines()):
-        row = l.strip().split('\t')
-        clip_ids.add(int(row[0]))
+if __name__ == "__main__":
+    args = get_args_parser().parse_args()
 
-print('=> collecting and cleaning subset captions')
-captioned = []
-uncaptioned = []
-with open('yfcc100m_dataset.txt') as f:
-    for l in tqdm(f.readlines()):
-        row = l.strip().split('\t')
-        if int(row[0]) in image_ids:
-            uncaptioned.append(int(row[0]))
-            if int(row[0]) in clip_ids:
-                title = unquote(row[8]).replace('+', ' ')
-                title = re.sub(cleanhtml, '', title)
-                title = re.sub(cleanurl, '', title)
+    DATASET = args.metadata
 
-                desc = unquote(row[9]).replace('+', ' ')
-                desc = re.sub(cleanhtml, '', desc)
-                desc = re.sub(cleanurl, '', desc)
-                
-                captioned.append((int(row[0]), title, desc))
+    cleanhtml = re.compile('<a.*?>|</a>|<b>|</b>|<i>|</i>')
+    cleanurl = re.compile('http\S+|www\S+')
 
-with open('yfcc15m.pkl', 'wb') as f:
-    pickle.dump(captioned, f)
+    print('=> loading YFCC image ids')
+    image_ids = np.load(args.images_ids)
+    image_ids = set(image_ids)
 
-with open('yfcc100m.pkl', 'wb') as f:
-    pickle.dump(uncaptioned, f)
+    print('=> loading CLIP image ids')
+    clip_ids = set()
+    with open(args.yfcc15m_tsv) as f:
+        for l in tqdm(f.readlines()):
+            row = l.strip().split('\t')
+            clip_ids.add(int(row[0]))
 
-print('Total captioned images:', len(captioned))  # 14689580
-print('Total uncaptioned images:', len(uncaptioned))  # 95920149
+    print('=> collecting and cleaning subset captions')
+    captioned = []
+    uncaptioned = []
+    with open('yfcc100m_dataset.txt') as f:
+        for l in tqdm(f.readlines()):
+            row = l.strip().split('\t')
+            if int(row[0]) in image_ids:
+                uncaptioned.append(int(row[0]))
+                if int(row[0]) in clip_ids:
+                    title = unquote(row[8]).replace('+', ' ')
+                    title = re.sub(cleanhtml, '', title)
+                    title = re.sub(cleanurl, '', title)
+
+                    desc = unquote(row[9]).replace('+', ' ')
+                    desc = re.sub(cleanhtml, '', desc)
+                    desc = re.sub(cleanurl, '', desc)
+                    
+                    captioned.append((int(row[0]), title, desc))
+
+    with open(os.path.join(args.output, 'yfcc15m.pkl'), 'wb') as f:
+        pickle.dump(captioned, f)
+
+    with open(os.path.join(args.output, 'yfcc100m.pkl'), 'wb') as f:
+        pickle.dump(uncaptioned, f)
+
+    print('Total captioned images:', len(captioned))  # 14689580
+    print('Total uncaptioned images:', len(uncaptioned))  # 95920149
