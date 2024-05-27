b829161185ff375db13601e599dfb48211504528
[jérémy morlier] <[jeremy.morlier@imt-atlantique.fr]>
2024-05-27 14:09:27 +0200

    test

--- script.sh
+++ script.sh
@@ -4,13 +4,6 @@
 #SBATCH --error=log/%j/errors.err # fichier d’erreur (%j = job ID)
 #SBATCH --partition=prepost
 #SBATCH --nodes=1 # reserver 1 nœud
-#SBATCH --ntasks=1 # reserver 4 taches (ou processus)
-#SBATCH --gres=gpu:4 # reserver 4 GPU
-#SBATCH --cpus-per-task=10 # reserver 10 CPU par tache (et memoire associee)
-#SBATCH --time=19:00:00 # temps maximal d’allocation "(HH:MM:SS)"
-#SBATCH --qos=qos_gpu-t3 # QoS
-#SBATCH --hint=nomultithread # desactiver l’hyperthreading
-#SBATCH --account=sxq@v100 # comptabilite V100
 
 module purge # nettoyer les modules herites par defaut
 conda deactivate # desactiver les environnements herites par defaut
